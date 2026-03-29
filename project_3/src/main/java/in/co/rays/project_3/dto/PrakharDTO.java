5e34540f3557b4336eabcce7d1d1bdecb7cc6709
nirajchopra <164307023+nirajchopra@users.noreply.github.com>
2026-03-30 00:21:59 +0530

    Project03

---
+++ project_3/src/main/java/in/co/rays/project_3/dto/PrakharDTO.java
@@ -0,0 +1,44 @@
+package in.co.rays.project_3.dto;
+
+import java.util.Date;
+
+public class PrakharDTO {
+
+	private String name;
+	private String login;
+	private String password;
+	private Date dob;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+}
